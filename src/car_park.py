98128dffe1096fcdf3db278e17140286ffca3dda
Tiffany Townsend <172116646+TZTownsend@users.noreply.github.com>
2024-11-19 11:58:07 +0800

    feat: Add register method to CarPark class

--- src/car_park.py
+++ src/car_park.py
@@ -1,5 +1,10 @@
+from sensor import Sensor
+from display import Display
+
+
 class CarPark:
-    def __init__(self, location='Unknown', capacity=0, plates=None, sensors=None, displays=None):
+    def __init__(self, location='Unknown', capacity=0, plates=None,
+                 sensors=None, displays=None):
         self.location = location
         self.capacity = capacity
         self.plates = plates or []
@@ -9,4 +14,11 @@ class CarPark:
     def __str__(self):
         return f"Car park at {self.location}, with {self.capacity} bays."
 
+    def register(self, component):
+        if not isinstance(component, (Sensor, Display)):
+            raise TypeError("Object must be a Sensor or Display")
+        if isinstance(component, Sensor):
+            self.sensors.append(component)
+        elif isinstance(component, Display):
+            self.displays.append(component)
 
